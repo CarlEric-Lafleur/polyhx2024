3483c170db30e530d866b83df1de0e7530651046
Antoine-Lafrance <antoine.lafrance@polymtl.ca>
2024-02-03 13:17:01 -0500

    added maps lib

--- client/transport/src/app/app.module.ts
+++ client/transport/src/app/app.module.ts
@@ -7,31 +7,13 @@ import {
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-<<<<<<< HEAD
 import { MapComponent } from './components/map/map.component';
+import { AgmCoreComponent } from '@agm/core';
 
 @NgModule({
   declarations: [AppComponent, MapComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
-=======
-import { HomeComponent } from './pages/home/home.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration(),
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
->>>>>>> 505ff1294bab7b908232850990910aed254fb73e
 })
 export class AppModule {}
