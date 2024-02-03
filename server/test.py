6d10805083c30aa13150d5f8bd68dba96c662c90
Kamilled <kamille.dg@hotmail.com>
2024-02-03 18:14:34 -0500

    J'ai ajouter des nouvelles features

--- server/test.py
+++ server/test.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
 utilisateurs = []
 courses = []
 
+photos = UploadSet("photos", IMAGES)
+app.config["UPLOADED_PHOTOS_DEST"] = "static/uploads/photos"  # Changer le chemin du répertoire
+configure_uploads(app, photos)
+
 class User:
-    def __init__(self, id, nom, description="", voiture="", est_electrique=False, rating=0, is_driver=False, points=0, rating_count=0, historique=[]):
+    def __init__(self, id, nom, description="", voiture="", est_electrique=False, rating=0, is_driver=False, points=0, rating_count=0, historique=[], numero_chauffeur="", photo=""):
         self.id = id
         self.nom = nom
         self.description = description
@@ -16,18 +21,21 @@ class User:
         self.points = points
         self.rating_count = rating_count
         self.historique = historique
+        self.numero_chauffeur = numero_chauffeur
+        self.photo = photo
 
 class Course:
-    def __init__(self, conducteur_id, passager_id, destination, cout_points, rating=None):
+    def __init__(self, conducteur_id, passager_id, destination, cout_points, rating=None, numero_chauffeur=""):
         self.conducteur_id = conducteur_id
         self.passager_id = passager_id
         self.destination = destination
         self.cout_points = cout_points
         self.rating = rating
+        self.numero_chauffeur = numero_chauffeur
 
 @app.route('/user', methods=['POST'])
 def ajouter_utilisateur():
-    data = request.get_json()
+    data = request.form
 
     id_utilisateur = len(utilisateurs) + 1
     nom = data['nom']
@@ -38,8 +46,14 @@ def ajouter_utilisateur():
     is_driver = data.get('is_driver', False)
     points = data.get('points', 0)
     rating_count = data.get('rating_count', 0)
+    numero_chauffeur = data.get('numero_chauffeur', '')
+    photo = None
 
-    nouvel_utilisateur = User(id_utilisateur, nom, description, voiture, est_electrique, rating, is_driver, points, rating_count)
+    if 'photo' in request.files:
+        # Sauvegarder la photo 
+        photo = photos.save(request.files['photo'], folder="photo")
+
+    nouvel_utilisateur = User(id_utilisateur, nom, description, voiture, est_electrique, rating, is_driver, points, rating_count, numero_chauffeur, photo)
     utilisateurs.append(nouvel_utilisateur.__dict__)
 
     return jsonify({'message': 'Utilisateur ajouté avec succès'})
@@ -47,13 +61,15 @@ def ajouter_utilisateur():
 @app.route('/user', methods=['GET'])
 def obtenir_utilisateurs():
     return jsonify({'utilisateurs': utilisateurs})
+
 @app.route('/course', methods=['POST'])
 def acheter_course():
-    data = request.get_json()
+    data = request.form
 
     conducteur_nom = data.get('conducteur', '')
     passager_nom = data.get('passager', '')
     destination = data.get('destination', '')
+    numero_chauffeur = data.get('numero_chauffeur', '')  
 
     conducteur = next((u for u in utilisateurs if u['nom'] == conducteur_nom), None)
     passager = next((u for u in utilisateurs if u['nom'] == passager_nom), None)
@@ -61,20 +77,22 @@ def acheter_course():
     if conducteur and passager:
         est_electrique_conducteur = conducteur.get('est_electrique', False)
 
-        # Assigner directement le coût en points en fonction du type de voiture
         cout_points = 75 if est_electrique_conducteur else 100
 
-        # Appliquer le coût pour le passager
         passager['points'] -= cout_points
 
-        # Appliquer les points pour le conducteur
         points_conducteur = 5/3*cout_points if est_electrique_conducteur else cout_points
         conducteur['points'] += points_conducteur
 
-        nouvelle_course = Course(conducteur['id'], passager['id'], destination, cout_points)
+       
+        nouvelle_course = Course(conducteur['id'], passager['id'], destination, cout_points, numero_chauffeur=numero_chauffeur)
         courses.append(nouvelle_course.__dict__)
 
-        # Ajouter les détails de la course à l'historique des utilisateurs
+       
+        course_existante = next((c for c in courses if c['conducteur_id'] == conducteur['id'] and c['passager_id'] == passager['id']), None)
+        if course_existante:
+            course_existante['numero_chauffeur'] = numero_chauffeur
+
         conducteur['historique'].append(nouvelle_course.__dict__)
         passager['historique'].append(nouvelle_course.__dict__)
 
@@ -111,31 +129,47 @@ def donner_rating():
 
 @app.route('/historique/<nom_utilisateur>', methods=['GET'])
 def obtenir_historique(nom_utilisateur):
-    # Trouver l'utilisateur avec le nom spécifié
+ 
     utilisateur = next((u for u in utilisateurs if u['nom'] == nom_utilisateur), None)
 
     if utilisateur:
-        # Renvoyer l'historique de l'utilisateur spécifié
+    
         return jsonify({'historique': utilisateur['historique']})
     else:
         return jsonify({'erreur': 'L\'utilisateur spécifié n\'existe pas'}), 400
 
-from flask import Flask, jsonify, send_file
-
-app = Flask(__name__)
-
 @app.route('/routes', methods=['GET'])
 def obtenir_route():
     try:
-        # Ouvrir et lire le contenu du fichier mapData.json
+        
         with open("mapData.json", "r") as f:
             contenu_json = f.read()
 
-        # Renvoyer le contenu en tant que réponse JSON
         return jsonify({'routes': contenu_json})
     except Exception as e:
-        # En cas d'erreur, renvoyer une réponse d'erreur
+       
         return jsonify({'erreur': str(e)}), 500
+
+@app.route('/courses_disponibles', methods=['GET'])
+def obtenir_courses_disponibles():
+   
+    courses_disponibles = [c for c in courses if c['passager_id'] is None]
+
+   
+    data_courses_disponibles = []
+    for course in courses_disponibles:
+        
+        conducteur = next((u for u in utilisateurs if u['id'] == course['conducteur_id']), None)
+
+        if conducteur:
+            data_courses_disponibles.append({
+                'conducteur_nom': conducteur['nom'],
+                'destination': course['destination'],
+                'cout_points': course['cout_points'],
+                'rating_conducteur': conducteur['rating'],
+            })
+
+    return jsonify({'courses_disponibles': data_courses_disponibles})
 
 if __name__ == '__main__':
     app.run(debug=True)
