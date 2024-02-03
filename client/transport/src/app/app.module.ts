50f695651199667ea592c0718999cc367b7f8e1c
CarlEric-Lafleur <carl-eric.lafleur@polymtl.ca>
2024-02-03 13:39:36 -0500

    material home

--- client/transport/src/app/app.module.ts
+++ client/transport/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MapComponent } from './components/map/map.component';
-import { AgmCoreComponent } from '@agm/core';
+// import { AgmCoreComponent } from '@agm/core';
+import { AppMaterialModule } from './material.module';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, MapComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, AppMaterialModule],
   providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
