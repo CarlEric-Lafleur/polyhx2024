3bc9235eaf4d66ff70ca3eab4e941a8a3268ff74
Antoine-Lafrance <antoine.lafrance@polymtl.ca>
2024-02-03 13:15:43 -0500

    created component folder

--- client/transport/src/app/app.module.ts
+++ client/transport/src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration(),
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MapComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration(), provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
